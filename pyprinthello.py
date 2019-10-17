2c4ae2829dec3eb87cd522124381f1867f98ff67
Naveen <naveen.hn@gmail.com>
2019-10-17 13:41:03 +0530

    Update pyprinthello.py

--- pyprinthello.py
+++ pyprinthello.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python
-print("Hello World!!)
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    application.run()
